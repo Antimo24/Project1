78ac55087ac9a0e5f72cfd6ef9ba9b4dfaba6640
Wenjie Liao <33884144+Antimo24@users.noreply.github.com>
2017-11-25 03:36:14 -0500

    update 2.0 :
    bug fixed, now this function needs instruction from the user to determine whether put the restriction on mono-increasing or not.

--- isValidData.m
+++ isValidData.m
@@ -1,4 +1,4 @@
-function output_data = isValidData(input_data)
+function output_data = isValidData(input_data, str_increasing)
 % isValidData: exaim if input data is valid
 % valid data type: nonempty real numeric column & nonnan
 % return input data if it is valid, two exceptions
@@ -18,24 +18,43 @@ function output_data = isValidData(input_data)
 %   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 %   See the License for the specific language governing permissions and
 %   limitations under the License.
+
+switch nargin
+    case 1
+        isIncreasing = logical(false);
+        str_increasing = 'none';
+    case 2
+        isIncreasing = strcmp(str_increasing, 'increasing');
+        if isIncreasing
+            str_increasing = 'increasing';
+        else
+            str_increasing = 'none';
+        end
+end
+
 try
+    if isIncreasing
     validateattributes(input_data, {'numeric'},...
         {'nonempty', 'column', 'real', 'nonnan', 'finite', 'increasing'})
+    else
+        validateattributes(input_data, {'numeric'},...
+        {'nonempty', 'column', 'real', 'nonnan', 'finite'})
+    end
     output_data = input_data;
 catch ME
     switch ME.identifier
         case 'MATLAB:expectedNonNaN'
             output_data = input_data(~any(isnan(input_data),2));
             warning('Invalid data type: Nan have been deleted.')
-            output_data = isValidData(output_data);
+            output_data = isValidData(output_data, str_increasing);
         case 'MATLAB:expectedFinite'
             output_data = input_data(~any(isinf(input_data),2));
             warning('Invalid data type: Nan have been deleted.')
-            output_data = isValidData(output_data);
+            output_data = isValidData(output_data, str_increasing);
         case 'MATLAB:expectedColumn'
             output_data = input_data.';
             warning('Invalid data type: row vector has been transposed to column vector.')
-            output_data = isValidData(output_data);
+            output_data = isValidData(output_data, str_increasing);
         otherwise
             output_data = [];
 %             rethrow(ME);
